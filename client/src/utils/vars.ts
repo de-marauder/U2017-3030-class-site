14d7220f80849d95d5b21c258c9bbeeccd038b96
de-marauder <ezikegodson@gmail.com>
2023-07-31 21:28:31 +0100

    fix: allowed U2015 and U2016

--- client/src/utils/vars.ts
+++ client/src/utils/vars.ts
@@ -1,6 +1,6 @@
 export const API_BASE_URL = `${import.meta.env.VITE_APP_API_BASE_URL_DEV}/api/v1`
 
-export const matNoRegex = /^U201[7, 8]\/30300[0-6][0-9]$/;
+export const matNoRegex = /^U201[5-8]\/30300[0-6][0-9]$/;
 export const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 export const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]+$/;
 export const phoneNumberRegex = /^[0-9]+$/;
